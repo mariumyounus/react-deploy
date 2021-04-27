b4b66f726473978716b190cbadacc745d56579f5
mumai <mariumyounus4@gmail.com>
2021-04-27 12:50:57 +1000

    deploy my app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import profileImage from "./img/ben.jpg";
+function App() {
+  return (
+    <div className="App">
+      <h1>Marium</h1>
+      <h1>author:marium</h1>
+      <img src={profileImage} alt="profileimage" />
+    </div>
+  );
+}
+
+export default App;
